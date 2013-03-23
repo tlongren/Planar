ba82c0253554522b82ff29ab4dff97373ff440b5
Stephen Sauceda <contact@stephensauceda.com>
2013-03-23 17:46:10 -0400

    style yo' comments fool!

--- article.php
+++ article.php
@@ -13,27 +13,25 @@
 		</section>
 
 		<?php if(comments_open()): ?>
-		<section class="comments">
+		<section class="comments wrap">
 			<?php if(has_comments()): ?>
 			<ul class="commentlist">
 				<?php $i = 0; while(comments()): $i++; ?>
 				<li class="comment" id="comment-<?php echo comment_id(); ?>">
-					<div class="wrap">
-						<h2><?php echo comment_name(); ?></h2>
+						<h5><?php echo comment_name(); ?></h5>
 						<time><?php echo relative_time(comment_time()); ?></time>
 
 						<div class="content">
 							<?php echo comment_text(); ?>
 						</div>
 
-						<span class="counter"><?php echo $i; ?></span>
-					</div>
+						<span class="counter"><small>#<?php echo $i; ?></small></span>
 				</li>
 				<?php endwhile; ?>
 			</ul>
 			<?php endif; ?>
 
-			<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
+			<form id="comment" class="commentform" method="post" action="<?php echo comment_form_url(); ?>#comment">
 				<?php echo comment_form_notifications(); ?>
 
 				<p class="name">
